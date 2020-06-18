6b95235404c74a3aaf1e27ddffc1338497d8c767
Hashiyaa <haochen213300@gmail.com>
2020-06-17 20:42:49 -0500

    Remove some unnecessary files

--- relaxed_ik_rust.py
+++ relaxed_ik_rust.py
@@ -4,7 +4,6 @@ import os
 import rospkg
 
 rospack = rospkg.RosPack()
-p = rospack.get_path('relaxed_ik_core')
+p = rospack.get_path('relaxed_ik_ros1')
 os.chdir(p + "/relaxed_ik_core")
-os.system('cargo run --bin relaxed_ik_node')
-
+os.system('cargo run --bin relaxed_ik_node_ros1')
